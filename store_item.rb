96af7a663835ee9fba514578905d4629bfd64166
YAYANYC <yaelgozin@me.com>
2017-01-09 20:52:29 -0500

    class

--- store_item.rb
+++ store_item.rb
@@ -5,4 +5,17 @@ fish={:color=>"gold", :fur=>"non",:tail=>"non"}
 cat={color: "gray", fur:"short", tail:"short"}
 dog={color:"black", fur:"long",tail:"chopped"}
 fish={color:"gold", fur:"non",tail:"non"}
-git remote add origin git@github.com:YAYANYC/store_item.rb.git
+
+class Cat
+  attr_reader :color, :fur, :tail
+  def initialize(input_color, input_fur, input_tail)
+    @color = input_color
+    @fur = input_fur
+    @stail = input_tail
+  end
+  def print_info
+    "#{color}, #{fur}, #{tail}"
+  end
+end
+cat1=Cat.new("red", "long", "long")
+puts cat1.color
