660230b4ab7ee766d73d00146cfa51a1cd480990
YAYANYC <yaelgozin@me.com>
2017-01-09 21:20:12 -0500

    hashes

--- store_item.rb
+++ store_item.rb
@@ -8,7 +8,22 @@ fish={color:"gold", fur:"non",tail:"non"}
 
 class Cat
   attr_reader :color, :fur, :tail
-  def initialize(input_color, input_fur, input_tail)
+    
+  def initialize(input_hash)
+    @color = input_hash[:color]
+    @fur = input_hash[:fur]
+    @stail = input_hash[:tail]
+  end
+  def print_info
+    "#{color}, #{fur}, #{tail}"
+  end
+end
+cat1=Cat.new(color:"red", fur:"long", tail:"long")
+puts cat1.color
+
+class Cat
+  attr_reader :color, :fur, :tail
+  def initialize(input_hash)
     @color = input_color
     @fur = input_fur
     @stail = input_tail
@@ -17,5 +32,3 @@ class Cat
     "#{color}, #{fur}, #{tail}"
   end
 end
-cat1=Cat.new("red", "long", "long")
-puts cat1.color
