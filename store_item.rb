ca41bc706b1bb50f02d4d1d646c33b92139b6ef3
YAYANYC <yaelgozin@me.com>
2017-01-09 20:04:45 -0500

    hashes

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+cat={:color=> "gray", :fur=>"short", :tail=>"short"}
+dog={:color=>"black", :fur=>"long",:tail=>"chopped"}
+fish={:color=>"gold", :fur=>"non",:tail=>"non"}
+
+cat={color: "gray", fur:"short", tail:"short"}
+dog={color:"black", fur:"long",tail:"chopped"}
+fish={color:"gold", fur:"non",tail:"non"}
+git remote add origin git@github.com:YAYANYC/store_item.rb.git
